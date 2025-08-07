fa4ad7a43f23cdb3e942397e564169d1bc4145a2
Michael Rohs <michael.rohs@hci.uni-hannover.de>
2025-08-07 14:43:13 +0200

    Back navigation via screens, not via ViewModels

---
+++ app/src/main/java/de/luh/hci/mi/myrecorder/ui/NavigateBack.kt
@@ -0,0 +1,17 @@
+package de.luh.hci.mi.myrecorder.ui
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+
+class NavigateBack(private val scope: CoroutineScope) {
+    private val channel = Channel<Unit>(Channel.BUFFERED)
+    val flow: Flow<Unit> = channel.receiveAsFlow()
+    fun trigger() {
+        scope.launch {
+            channel.send(Unit)
+        }
+    }
+}
